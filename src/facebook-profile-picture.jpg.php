33f51aed80bb288023773b473476cd9d61bb40fb
acj <gitlab@acjs.net>
2019-11-03 15:16:28 +0100

    Facebook profile picture.

--- src/facebook-profile-picture.jpg.php
+++ src/facebook-profile-picture.jpg.php
@@ -2,6 +2,6 @@
 
 require_once 'class.deicon.php';
 
-$im = new Deicon(['height' => 960, 'width' => 960, 'type' => 'jpg']);
+$im = new Deicon([/*'height' => 960, 'width' => 960, */'type' => 'jpg']);
 if(isset($_GET['save'])) $im->save();
 echo $im;
--- src/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'class.deicon.php';
-
-$test = new Deicon;
-
-//$test->input = [];
-
-echo $test;
