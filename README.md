480e52f54587d992614a8436fd25be1567328cc0
ACJ <gitlab@acjs.net>
2021-03-20 20:15:14 +0100

    Checkboxes to array PoC

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Iconen van [deidee](https://deidee.nl/) worden op de server gegenereerd, maar bi
 
 ## TODO
 
-- weg kunnen schrijven naar een bitmapformaat als PNG;
-- weg kunnen schrijven naar een vectorformaat als SVG;
-- het aantal rijen en kolommen kunnen aanpassen;
-- bij het aan- of uitzetten van een blokje de _favicon_ automatisch updaten;
-- bij het aan- of uitzetten van een blokje de URL automatisch updaten;
-- actieve blokjes voorzien van de afwijking rechts en onder zoals dat bij het logo ook gebeurt;
-- link kunnen delen naar specifiek icoon;
-- eindformaat in kunnen stellen.
+- [ ] weg kunnen schrijven naar een bitmapformaat als PNG;
+- [ ] weg kunnen schrijven naar een vectorformaat als SVG;
+- [ ] het aantal rijen en kolommen kunnen aanpassen;
+- [ ] bij het aan- of uitzetten van een blokje de _favicon_ automatisch updaten;
+- [ ] bij het aan- of uitzetten van een blokje de URL automatisch updaten;
+- [ ] actieve blokjes voorzien van de afwijking rechts en onder zoals dat bij het logo ook gebeurt;
+- [ ] link kunnen delen naar specifiek icoon;
+- [ ] eindformaat in kunnen stellen.
---
+++ sandbox/input-to-text.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types = 1);
+
+namespace deidee;
+
+define('DEFAULT_COLS', 16);
+define('DEFAULT_ROWS', 16);
+define('DEFAULT_DATA', []);
+
+$cols = !empty($_GET['cols']) ? intval($_GET['cols']) : DEFAULT_COLS;
+$rows = !empty($_GET['rows']) ? intval($_GET['rows']) : DEFAULT_ROWS;
+
+$empty_row = array_fill(0, $cols, '0');
+$empty_set = array_fill(0, $rows, $empty_row);
+
+$data = !empty($_GET['data']) && is_array($_GET['data']) ? $_GET['data'] : DEFAULT_DATA;
+
+$matrix = array_replace_recursive($empty_set,
+    array_intersect_key(
+        $data, $empty_set
+    )
+);
+
+// Just a counter.
+$cell = 0;
+
+?>
+<!doctype html>
+<html dir="ltr" lang="nl">
+<head>
+    <meta charset="utf-8">
+    <title>HetIcoon</title>
+</head>
+<body id="top">
+<form action="">
+    <table>
+        <tbody>
+        <?php for($r = 0; $r < $rows; $r++): ?>
+        <tr>
+            <?php for($c = 0; $c < $cols; $c++): $cell++; ?>
+            <td><label><input<?= (!empty($matrix[$r][$c]) ? ' checked' : '') ?> id="cell-<?= $cell ?>" name="data[<?= $r ?>][<?= $c ?>]" type="checkbox" value="<?= $cell ?>"></label></td>
+            <?php endfor; ?>
+        </tr>
+        <?php endfor; ?>
+        </tbody>
+    </table>
+    <button type="submit">Doe</button>
+</form>
+<pre>
+<?= htmlspecialchars(print_r($matrix, true)) ?>
+</pre>
+</body>
+</html>
