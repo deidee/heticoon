9a6b96be0f2019532ceb622d93bad5f11853cac4
ACJ <gitlab@acjs.net>
2022-12-16 17:27:58 +0100

    Update size

--- src/instagram-profile-picture.jpg.php
+++ src/instagram-profile-picture.jpg.php
@@ -5,7 +5,7 @@ namespace deidee;
 
 require_once 'class.deicon.php';
 
-$im = new Deicon([/*'height' => 960, 'width' => 960, */'type' => 'jpg']);
+$im = new Deicon(['size' => 64, 'type' => 'jpg']);
 $im->setOffset(1);
 if(isset($_GET['save'])) $im->save();
 echo $im;
