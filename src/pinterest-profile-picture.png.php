b380dd144c5d438b085a635921479abe0195e78d
ACJ <gitlab@acjs.net>
2022-12-29 13:36:51 +0100

    Add _Pinterest_ profile picture generator

---
+++ src/pinterest-profile-picture.png.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+require_once dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+use deidee\heticoon\Deicon;
+
+$im = new Deicon([/*'height' => 960, 'width' => 960, */'size' => 48, 'type' => 'jpg']);
+$im->setOffset(1);
+$im->setPadding(2);
+
+if(isset($_GET['save'])) $im->save();
+echo $im;
