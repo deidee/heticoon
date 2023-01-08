be922a6f2167357bb28db3ba3a6e8bf9adb5f6ae
ACJ <gitlab@acjs.net>
2023-01-08 23:35:34 +0100

    Add inline SVG test (which is not very good)

---
+++ src/inline-svg.html.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+require_once dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+use deidee\heticoon\Deicon;
+
+$im = new Deicon([/*'height' => 960, 'width' => 960, */'size' => 48, 'type' => 'svg']);
+$im->setOffset(1);
+$im->setPadding(2);
+
+?>
+<!doctype html>
+<title>Inline SVG</title>
+<?= $im ?>
