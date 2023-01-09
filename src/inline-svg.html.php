7eb1473903aae3bedec8c1e84ac6c3fab2c855b6
ACJ <gitlab@acjs.net>
2023-01-09 15:42:56 +0100

    No pads for you!

--- src/inline-svg.html.php
+++ src/inline-svg.html.php
@@ -8,7 +8,7 @@ use deidee\heticoon\Deicon;
 
 $im = new Deicon([/*'height' => 960, 'width' => 960, */'size' => 48, 'type' => 'svg']);
 $im->setOffset(1);
-$im->setPadding(2);
+$im->setPadding(0);
 
 ?>
 <!doctype html>
