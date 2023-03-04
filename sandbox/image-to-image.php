f3bcbcfca17fb1b65ba7222b037d62d952cc62c2
ACJ <gitlab@acjs.net>
2023-03-04 15:46:29 +0100

    Convert pixel colors to data array PoC

--- sandbox/image-to-image.php
+++ sandbox/image-to-image.php
@@ -17,8 +17,33 @@ if(strtoupper($_SERVER['REQUEST_METHOD']) === 'POST') {
 
     if(is_uploaded_file($_FILES['source']['tmp_name'])) {
         echo '<div>' . htmlspecialchars($_FILES['source']['name']) . ':</div>';
+
+        $img = new Imagick();
+        $file = fopen($_FILES['source']['tmp_name'], 'rb');
+        $img->readImageFile($file);
+        $ita = $img->getPixelIterator();
+        $data = [];
+        $y = 0;
+
+        foreach ($ita as $row => $pixels) { /* Loop through pixel rows */
+            $data[$y] = [];
+
+            foreach ($pixels as $column => $pixel) { /* Loop through the pixels in the row (columns) */
+                /** @var $pixel \ImagickPixel */
+                $c = $pixel->getColor();
+                // Check if white and/or transparent.
+                if(empty($c['a']) OR ($c['r'] === 255 && $c['g'] === 255) && $c['b'] === 255) {
+                    $data[$y][] = 0;
+                } else {
+                    $data[$y][] = 1;
+                }
+            }
+            $y++;
+            $ita->syncIterator(); /* Sync the iterator, this is important to do on each iteration */
+        }
+
         echo '<pre>';
-        echo base64_encode($_FILES['source']['tmp_name']);
+        echo var_dump($data);
         echo '</pre>';
     } else {
         echo '<p>Geen bronbestand gevonden.</p>';
